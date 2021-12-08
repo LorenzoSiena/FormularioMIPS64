928824e1a8904ddf88f2b71f41335dba9bbd063e
LorenzoSiena <74120782+LorenzoSiena@users.noreply.github.com>
2021-12-08 17:19:29 +0100

    Update main.s

--- main.s
+++ main.s
@@ -10,3 +10,23 @@ daddi $t0,r0,STR
 sd $t0,ind_sys3(r0)
 daddi $14,r0,p1_sys3
 syscall 3             #->r1= dimensione stringa
+
+
+
+
+SYSCALL 3: READ
+
+.data
+buffer: .space 8
+par: .space 8  #descrittore
+ind: .space 8  #indirizzo di partenza
+num_byte: .word 8
+
+.code
+
+sd r0,par(r0)   # stdin
+daddi $t0, r0, buffer # copia il valore buffer in $t0
+sd $t0, ind(r0) # salva l’indirizzo buffer in ind
+daddi r14, r0, par
+syscall 3       #->r1= stringa inserita
+
