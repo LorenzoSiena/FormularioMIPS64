b829cca2911cde63e6fb30f13852171bc34f6657
LorenzoSiena <74120782+LorenzoSiena@users.noreply.github.com>
2021-12-08 17:10:41 +0100

    Update main.s

--- main.s
+++ main.s
@@ -4,8 +4,9 @@
 
 .code
 
-#CALCOLO LA LUNGHEZZA DELLA STRINGA
-#scanf("%s",STR) SYS3
+                #CALCOLO LA LUNGHEZZA DELLA STRINGA
+#scanf("%s",STR)->SYS3
 daddi $t0,r0,STR
 sd $t0,ind_sys3(r0)
 daddi $14,r0,p1_sys3
+syscall 3             #->r1= dimensione stringa
